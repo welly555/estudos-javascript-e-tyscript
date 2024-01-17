a9b42f624739bc84334f85ad28bf2d2d57cda3b6
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2024-01-17 20:09:01 -0300

    funcionalidade de registrar user

--- projetoAgenda/src/controllers/loginController.js
+++ projetoAgenda/src/controllers/loginController.js
@@ -4,15 +4,26 @@ exports.index = (req,res) =>{
     res.render('login')
 }
 exports.register = async (req,res) =>{
-    const login = new Login(req.body)
-    await login.register()
 
-    if( login.errors.length > 0){
-        req.flash('errors', login.errors)
+    try{
+        const login = new Login(req.body)
+        await login.register()
+    
+        if( login.errors.length > 0){
+            req.flash('errors', login.errors)
+            req.session.save(function() {
+                return res.redirect('/login/index')
+            })
+            return
+        }
+
+        req.flash('success', 'Usuario criado com sucesso')
         req.session.save(function() {
-            return res.redirect('back')
+            return res.redirect('/login/index')
         })
-        return
+        
+    }catch(e) {
+        return res.render('404')
     }
-    res.send(login.errors)
+    
 }
--- projetoAgenda/src/models/loginModel.js
+++ projetoAgenda/src/models/loginModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+const bcryptjs = require('bcryptjs')
 
 const LoginSchema = new mongoose.Schema({
     email: {type: String, require:true},
@@ -19,12 +20,21 @@ class Login {
         this.valida()
         if (this.errors.length > 0) return
 
+        await this.userExists()
+        if (this.errors.length > 0) return
         try{
+            const salt = bcryptjs.genSaltSync()
+            this.body.password = bcryptjs.hashSync(this.body.password, salt)
             this.user = await LoginModel.create(this.body)
         } catch (e) {
             console.log(e)
         }
         
+    }
+
+    async userExists() {
+        const user = await LoginModel.findOne({email: this.body.email})
+        if(user) this.errors.push('Usuario já cadastrado no sistema')
     }
 
     valida() {
