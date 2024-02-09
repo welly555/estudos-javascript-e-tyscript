c3b3fd48c76468cb77aee68f6a6a444e5b9a65d9
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2024-02-09 19:31:03 -0300

    update security

--- api_rest/src/controllers/UserController.js
+++ api_rest/src/controllers/UserController.js
@@ -4,7 +4,8 @@ class UserController {
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      return res.json(novoUser);
+      const { id, nome, email } = novoUser;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -14,9 +15,7 @@ class UserController {
 
   async index(req, res) {
     try {
-      const users = await User.findAll();
-      console.log('USER ID', req.userId);
-      console.log('USER EMAIL', req.userEmail);
+      const users = await User.findAll({ attributes: ['id', 'nome', 'email'] });
       return res.json(users);
     } catch (e) {
       return res.json(null);
@@ -26,7 +25,9 @@ class UserController {
   async show(req, res) {
     try {
       const user = await User.findByPk(req.params.id);
-      return res.json(user);
+
+      const { id, nome, email } = user;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.json(null);
     }
@@ -34,14 +35,7 @@ class UserController {
 
   async update(req, res) {
     try {
-      const { id } = req.params;
-
-      if (!id) {
-        return res.status(400).json({
-          errors: ['ID não enviado'],
-        });
-      }
-      const user = await User.findByPk(id);
+      const user = await User.findByPk(req.userId);
       if (!user) {
         return res.status(400).json({
           errors: ['Usuario não existe'],
@@ -49,7 +43,8 @@ class UserController {
       }
 
       const novosDados = await user.update(req.body);
-      return res.json(novosDados);
+      const { id, nome, email } = novosDados;
+      return res.json({ id, nome, email });
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
@@ -59,14 +54,7 @@ class UserController {
 
   async delete(req, res) {
     try {
-      const { id } = req.params;
-
-      if (!id) {
-        return res.status(400).json({
-          errors: ['ID não enviado'],
-        });
-      }
-      const user = await User.findByPk(id);
+      const user = await User.findByPk(req.userId);
       if (!user) {
         return res.status(400).json({
           errors: ['Usuario não existe'],
@@ -74,7 +62,7 @@ class UserController {
       }
 
       await user.destroy();
-      return res.json(user);
+      return res.json(null);
     } catch (e) {
       return res.status(400).json({
         errors: e.errors.map((err) => err.message),
--- api_rest/src/routes/userRoutes.js
+++ api_rest/src/routes/userRoutes.js
@@ -5,10 +5,11 @@ import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router();
 
+// router.get('/', UserControllers.index);
+// router.get('/:id', UserControllers.show);
+// em cima inutil em baixo util
 router.post('/', UserControllers.store);
-router.get('/', loginRequired, UserControllers.index);
-router.get('/:id', UserControllers.show);
-router.put('/:id', UserControllers.update);
-router.delete('/:id', UserControllers.delete);
+router.put('/', loginRequired, UserControllers.update);
+router.delete('/', loginRequired, UserControllers.delete);
 
 export default router;
