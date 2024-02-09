fe09cf1f2639beaef1ba70b3e2b17299f334e7fe
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2024-02-09 18:41:07 -0300

    token authorized

--- api_rest/src/routes/userRoutes.js
+++ api_rest/src/routes/userRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import UserControllers from '../controllers/UserController';
 
+import loginRequired from '../middlewares/loginRequired';
+
 const router = new Router();
 
 router.post('/', UserControllers.store);
-router.get('/', UserControllers.index);
+router.get('/', loginRequired, UserControllers.index);
 router.get('/:id', UserControllers.show);
 router.put('/:id', UserControllers.update);
 router.delete('/:id', UserControllers.delete);
