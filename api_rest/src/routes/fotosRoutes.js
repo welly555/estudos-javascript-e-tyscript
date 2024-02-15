8011470bc50ddad4ba974a1ddaabd9e4bc773538
welly555 <wellyngton.targino@aluno.uepb.edu.br>
2024-02-15 20:05:59 -0300

    validate files photos

--- api_rest/src/routes/fotosRoutes.js
+++ api_rest/src/routes/fotosRoutes.js
@@ -1,13 +1,9 @@
 import { Router } from 'express';
-import multer from 'multer';
 
 import FotoControllers from '../controllers/FotoControllers';
-import multerConfig from '../config/multerConfig';
-
-const upload = multer(multerConfig);
 
 const router = new Router();
 
-router.post('/', upload.single('foto'), FotoControllers.store);
+router.post('/', FotoControllers.store);
 
 export default router;
